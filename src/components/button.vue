e745b0dc79e7ac3b080cb97aedaafc81ad3c13c6
Naufaldwar <naufaldwiariananto@gmail.com>
2022-09-15 02:05:22 +0700

    fisrt commit make button component

---
+++ src/components/button.vue
@@ -0,0 +1,3 @@
+<template>
+    <button class="bg-slate-600 rounded-md font-medium text-white px-5 py-2">Tweet</button>
+</template>
--- src/components/formTweet.vue
+++ src/components/formTweet.vue
@@ -2,7 +2,17 @@
     <div class="flex justify-center">
         <div class="w-1/2 items-center border border-slate-400 rounded-xl h-44 grid justify-center">
             <textarea name="" id="" cols="67" rows="3" placeholder="Wite Something" class="border border-slate-400 p-3 mt-2 rounded-md" autofocus></textarea>
-            
+            <Button class="justify-self-end"></Button>
         </div>
     </div>
 </template>
+
+<script>
+
+import Button from './button.vue'
+export default{
+    components:{
+        Button,
+    }
+}
+</script>
