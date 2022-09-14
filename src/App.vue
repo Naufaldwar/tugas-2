cc71eddefa9afa453342ad11877bf0f204a3e582
naufal <naufaldwiariananto@gmail.com>
2022-09-14 18:34:23 +0700

    first commit add navbar component

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import Navbar from "./components/navbar.vue";
+import poto from './assets/nopal.jpg'
+
+export default {
+  components: {
+    Navbar,
+  },
+  data(){
+    return{
+      nama:'Naufal Dwi Ariananto',
+      photo:poto,
+      nickname:'@nopaldwar'
+    }
+    
+  },
+  provide(){
+    return{
+      nama: this.nama,
+      photo: this.photo,
+      nick: this.nickname
+    }
+  }
+};
+</script>
+
+<template>
+  
+    <Navbar />
+  
+</template>
+
---
+++ src/components/navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="bg-[#00bd7e] rounded-bl-md rounded-br-md h-14 flex justify-center"
+  >
+    <div class="w-1/2 flex justify-between items-center">
+      <p class="font-medium text-slate-200">Simple Twitter Clone with Vue</p>
+      <div class="grid grid-flow-col  ">
+        <p class="font-medium text-slate-100 col-span-2" >{{nama}}</p>
+        <p class="col-span-2 text-end">@naufal</p>
+        <img :src="poto" alt="avatar1" class="max-w-[50px] rounded-full ml-4 row-span-2">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+
+export default{inject:['nama','photo'],
+
+data(){
+  return{poto:this.photo}
+  
+}}
+    
+
+</script>
