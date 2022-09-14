2c9d702fb1090fd945520c835470b5ec30f1b565
Naufaldwar <naufaldwiariananto@gmail.com>
2022-09-15 00:36:08 +0700

    first commit make formTweet

---
+++ src/components/formTweet.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex justify-center">
+        <div class="w-1/2 items-center border border-slate-400 rounded-xl h-44 grid justify-center">
+            <textarea name="" id="" cols="67" rows="3" placeholder="Wite Something" class="border border-slate-400 p-3 mt-2 rounded-md" autofocus></textarea>
+            
+        </div>
+    </div>
+</template>
