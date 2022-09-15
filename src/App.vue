e694ec1e4b4d3d9fdfe687b1402a7c51fb1833c8
Naufaldwar <naufaldwiariananto@gmail.com>
2022-09-15 17:31:47 +0700

    first commit FeedCard component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,51 @@
 <script>
 import Navbar from "./components/Navbar.vue";
-import poto from './assets/nopal.jpg'
-import FormTweet from './components/FormTweet.vue'
+import poto from "./assets/nopal.jpg";
+import FormTweet from "./components/FormTweet.vue";
+import FeedCard from "./components/FeedCard.vue";
 
 export default {
   components: {
     Navbar,
     FormTweet,
-},
-  data(){
-    return{
-      nama:'Naufal Dwi Ariananto',
-      photo:poto,
-      nickname:'@nopaldwar'
-    }
-    
+    FeedCard,
   },
-  provide(){
-    return{
-      nama: this.nama,
-      photo: this.photo,
-      nick: this.nickname
-    }
-  }
+  data() {
+    return {
+      user: {
+        nama: "Naufal Dwi Ariananto",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSucE8cRhaQTngU1IJgSw7IB5poXfQNoTsGLf-RXRvaik-z6fieK13YwlYqLWHPTCzEWEE&usqp=CAU",
+        nickname: "@nopaldwar",
+      },
+      feeds: [
+        {
+          user: "dwi",
+          avatar:"",
+          usernick: "@dwi",
+          tweet:"Géritole de maudite marde de saint-cimonaque de cul de purée de charrue de cossin de saint-ciboire de baptême de gériboire de viarge de mosus de tabarouette."
+        },
+        {
+          user: "ari",
+          usernick: "@ari",
+          tweet:"Sapristi de doux Jésus de cibouleau de colon de cochonnerie de sainte-viarge de purée de taboire de câlique de mosus de calvinouche d'étole."
+        },
+      ],
+    };
+  },
+
+  provide() {
+    return {
+      nama: this.user.nama,
+      photo: this.user.photo,
+      nick: this.user.nickname,
+    };
+  },
 };
 </script>
 
 <template>
-  <Navbar/>
-  <FormTweet class="mt-8"/>
+  <Navbar />
+  <FormTweet class="mt-8" />
+  <h1 class="w-1/2 flex mx-auto my-4 text-xl text-slate-600">Feeds</h1>
+  <FeedCard :feeds="feeds" :key="feed"></FeedCard>
 </template>
-
---
+++ src/components/FeedCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="w-1/2 flex mx-auto ">
+        <p v-for="feed in feeds" :key="feed">{{feed.user}}</p>
+    </div>
+
+</template>
+<script>
+    export default{
+        props:{
+            feeds: Array
+        }
+    }
+</script>
